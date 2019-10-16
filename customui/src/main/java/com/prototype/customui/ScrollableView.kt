fff774dbbd44cc9bce1aaffc5730028dd3236d25
Pro Dev <prodev9999@gmail.com>
2019-10-17 05:20:34 +0800

    Added ScrollableView component

---
+++ customui/src/main/java/com/prototype/customui/ScrollableView.kt
@@ -0,0 +1,9 @@
+package com.prototype.customui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.ScrollView
+
+class ScrollableView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : ScrollView(context, attrs, defStyleAttr)
