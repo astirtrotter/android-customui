f90cdca8661367ce43c376a0c1b99f13b0ec763d
Pro Dev <prodev9999@gmail.com>
2019-10-18 01:14:40 +0800

    Added common-button structure

---
+++ customui/src/main/java/com/prototype/customui/CommonButton.kt
@@ -0,0 +1,28 @@
+package com.prototype.customui
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.drawable.Drawable
+import android.text.TextPaint
+import android.util.AttributeSet
+import android.view.View
+import com.prototype.customui.base._BaseCustomUIComponent
+
+class CommonButton @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : _BaseCustomUIComponent(context, attrs, defStyleAttr) {
+
+    override val layoutResId: Int
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+    override fun fetchLayout(container: View) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun readAttr(attrs: AttributeSet?, defStyle: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
