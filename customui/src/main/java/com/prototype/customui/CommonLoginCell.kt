9edff91862cefd36429c3a86d0b6ab240f86f0d0
Pro Dev <prodev9999@gmail.com>
2019-10-17 23:16:51 +0800

    Implemented common-login-cell

--- customui/src/main/java/com/prototype/customui/CommonLoginCell.kt
+++ customui/src/main/java/com/prototype/customui/CommonLoginCell.kt
@@ -8,6 +8,9 @@ import android.graphics.drawable.Drawable
 import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.TextView
 import com.prototype.customui.base._BaseCustomUIComponent
 
 class CommonLoginCell @JvmOverloads constructor(
@@ -17,11 +20,49 @@ class CommonLoginCell @JvmOverloads constructor(
     override val layoutResId: Int
         get() = R.layout.layout_common_login_cell
 
-    override fun fetchLayout(container: View) {
+    var labelText: String = ""
+        set(value) {
+            field = value
+            label.text = value
+        }
+    var hintText: String? = ""
+        set(value) {
+            field = value
+            input.hint = value
+        }
+    var warningText: String? = null
+        set(value) {
+            field = value
+            warning.text = value
+            setVisibilityWithValue(warning, value)
+        }
+    var isPassword: Boolean = false
+        set(value) {
+            field = value
+            input.inputType = EditorInfo.TYPE_TEXT_VARIATION_PASSWORD
+        }
 
+    private lateinit var label: TextView
+    private lateinit var input: EditText
+    private lateinit var warning: TextView
+
+    override fun fetchLayout(container: View) {
+        label = container.findViewById(R.id.login_label)
+        input = container.findViewById(R.id.login_input)
+        warning = container.findViewById(R.id.login_warning)
     }
 
     override fun readAttr(attrs: AttributeSet?, defStyle: Int) {
+        val a = context.obtainStyledAttributes(
+            attrs, R.styleable.CommonLoginCell, defStyle, 0
+        )
+
+        labelText = a.getString(R.styleable.CommonLoginCell_labelText) ?: "Label"
+        hintText = a.getString(R.styleable.CommonLoginCell_hintText)
+        warningText = a.getString(R.styleable.CommonLoginCell_warningText)
+        isPassword = a.getBoolean(R.styleable.CommonLoginCell_isPassword, false)
+
+        a.recycle()
     }
 
 }
