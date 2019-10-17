aa4a91256d634b64f285333f0cf4d5be1f4cbb83
Pro Dev <prodev9999@gmail.com>
2019-10-18 01:37:00 +0800

    Implemented common-button

--- customui/src/main/java/com/prototype/customui/CommonButton.kt
+++ customui/src/main/java/com/prototype/customui/CommonButton.kt
@@ -8,21 +8,61 @@ import android.graphics.drawable.Drawable
 import android.text.TextPaint
 import android.util.AttributeSet
 import android.view.View
+import android.widget.TextView
 import com.prototype.customui.base._BaseCustomUIComponent
 
 class CommonButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : _BaseCustomUIComponent(context, attrs, defStyleAttr) {
 
+    companion object {
+        private val enabledColor = 0xFFFF0000.toInt()
+        private val disabledColor = 0xFFCCCCCC.toInt()
+    }
+
     override val layoutResId: Int
-        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        get() = R.layout.layout_common_button
+
+    var enabledColor: Int = CommonButton.enabledColor
+    var disabledColor: Int = CommonButton.disabledColor
+    var displayText: String? = ""
+        set(value) {
+            field = value
+            display.text = displayText
+        }
+    var textColor: Int = Color.WHITE
+        set(value) {
+            field = value
+            display.setTextColor(value)
+        }
+
+    private lateinit var display: TextView
 
     override fun fetchLayout(container: View) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        display = container.findViewById(R.id.button_text)
     }
 
     override fun readAttr(attrs: AttributeSet?, defStyle: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val a = context.obtainStyledAttributes(
+            attrs, R.styleable.CommonButton, defStyle, 0
+        )
+
+        enabledColor = a.getColor(R.styleable.CommonButton_enabledButtonColor, CommonButton.enabledColor)
+        disabledColor = a.getColor(R.styleable.CommonButton_disabledButtonColor, CommonButton.disabledColor)
+        displayText = a.getString(R.styleable.CommonButton_displayText)
+        textColor = a.getColor(R.styleable.CommonButton_textColor, Color.WHITE)
+
+        a.recycle()
+
+    }
+
+    override fun setEnabled(enabled: Boolean) {
+        super.setEnabled(enabled)
+        setButtonColor()
+    }
+
+    private fun setButtonColor() {
+        setBackgroundColor(enabledColor.takeIf { isEnabled } ?: disabledColor)
     }
 
 }
