7b99be1bcee759d32ca8c1945755e5eafba5654d
Pro Dev <prodev9999@gmail.com>
2019-10-18 02:25:25 +0800

    Added footer structure

---
+++ customui/src/main/java/com/prototype/customui/CommonFooterWithTextAndButton.kt
@@ -0,0 +1,23 @@
+package com.prototype.customui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import com.prototype.customui.base._BaseCustomUIComponent
+
+class CommonFooterWithTextAndButton @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : _BaseCustomUIComponent(context, attrs, defStyleAttr) {
+
+    override val layoutResId: Int
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+
+    override fun fetchLayout(container: View) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun readAttr(attrs: AttributeSet?, defStyle: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
